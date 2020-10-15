402f59b019f7c745b7c2671295b5c011d4d6a0f6
Rebecca Mead <beckyleemead@gmail.com>
2020-10-14 20:41:34 -0400

    set view with top-level buttons

--- src/App.js
+++ src/App.js
@@ -8,9 +8,21 @@ class App extends React.Component {
     this.state = {
       itemCount: 0,
       compartment: null,
-      itemsToShow: 'all',
-    }
+      itemView: 'all',
+    };
+    this.viewButtons = [
+      'all',
+      'to do',
+      'done'
+    ];
+    this.setView = this.setView.bind(this);
+    console.log(window.localStorage);
     // this.addListItem = this.addListItem.bind(this)
+  }
+
+  // declare a function to change view with viewButtons
+  setView(view) {
+    this.setState({ itemView: view })
   }
 
   // declare an array with compartment background colors
@@ -18,15 +30,14 @@ class App extends React.Component {
 
   // declare storage object with LocalStorage
   // let data = {
-    // compartmentName: [
-      // {
-        // listItemName: 'string input by user',
-        // id: itemCount,
-        // done: false
-      // }
-    // ]
+  // compartmentName: [
+  // {
+  // listItemName: 'string input by user',
+  // id: itemCount,
+  // done: false
   // }
-
+  // ]
+  // }
 
   // declare function addCompartment to add a new compartment array to the storage object
   // to be used on compartment input button click
@@ -36,8 +47,8 @@ class App extends React.Component {
 
   // declare function addListItem to add a new list item object to the storage object / compartment array
   // to be used on list input button click (add to buttons: onClick={this.addListItem.bind(this)}) ???
-    // this.setState({itemCount: this.state.itemCount + 1});
-    // add list item object to compartment's array
+  // this.setState({itemCount: this.state.itemCount + 1});
+  // add list item object to compartment's array
 
   // declare function delListItem to remove a list item object from the storage object / compartment array
   // to be used on list X button click
@@ -47,15 +58,29 @@ class App extends React.Component {
       <>
         {/* header */}
         <h1>Compartments</h1>
-        {/* add function to the following buttons to setState of itemsToShow */}
-        <button>All</button><button>To Do</button><button>Completed</button>
+        {/* render buttons that will setState of itemView */}
+        {
+          // generate buttons dynamically using props.pages
+          this.viewButtons.map((item) => {
+            return (
+              <button
+                className={'btn ' + ((this.state.itemView === item) ? ' btn-primary' : 'btn-secondary')}
+                onClick={() => this.setView(item)}
+              >
+                {item}
+              </button>
+              //                onClick={() => props.setPage(index)}
+            )
+          })
+        }
+
         <Compartment compartmentName='fun' />
         {/* 
         COMPARTMENT INPUT onClick=addCompartment
 
         for each compartment in the data object:
             <Compartment
-              itemsToShow={this.state.itemsToShow}
+              itemView={this.state.itemView}
               compartmentName={name}
               data={data.compartmentName}
               addListItem={(???) => this.addListItem(???)}
