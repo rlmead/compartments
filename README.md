ba1374ba0c04f5d93b5518c6d0d7573a88495598
Rebecca Mead <beckyleemead@gmail.com>
2020-10-15 11:53:22 -0400

    addCompartment function, minor fixes

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ An app for organizing your busy mind
 </br></br>
 | MOSCOW | |
 | - | - |
-| must have | dynamic rendering</br>mobile responsiveness</br>store data using localStorage</br>add to-do items</br>cross off to-do items</br>show items filtered by all/to do/done</br></br> |
-| should have | delete items</br>divide tasks into compartment sub-lists</br>complete all items in compartment</br>mark all items in compartment incomplete</br>delete all items in compartment</br> |
+| must have | dynamic rendering</br>mobile responsiveness</br>store data using localStorage</br>add to-do items</br>cross off to-do items</br>show items filtered by all/to do/done |
+| should have | divide tasks into compartment sub-lists</br>delete items</br>complete all items in compartment</br>mark all items in compartment incomplete</br>delete all items in compartment</br> |
 | could have | collapse compartments accordion-style</br> |
 | won't have |  |
--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends React.Component {
       'done'
     ];
     this.setView = this.setView.bind(this);
-    console.log(window.localStorage);
+    this.addCompartment = this.addCompartment.bind(this);
     // this.addListItem = this.addListItem.bind(this)
   }
 
@@ -24,6 +24,23 @@ class App extends React.Component {
   setView(view) {
     this.setState({ itemView: view })
   }
+
+  // declare function addCompartment to add a new compartment array to the storage object
+  // to be used on compartment input button click - and componentDidMount?
+  addCompartment(compartmentName) {
+    window.localStorage.setItem(compartmentName,'');
+    // console.log(localStorage);
+  }
+  
+  // declare function addListItem to add a new list item object to the storage object / compartment array
+  // to be used on list input button click (add to buttons: onClick={this.addListItem.bind(this)}) ???
+  // this.setState({itemCount: this.state.itemCount + 1});
+  // add list item object to compartment's array
+  // addListItem(compartmentName) {
+  //   let existingToDos = (localStorage.getItem(compartmentName)) : JSON.parse(localStorage.getItem(compartmentName)) : [];
+  //   console.log(existingToDos);
+  // }
+  
 
   // declare an array with compartment background colors
   // to alternate when generating compartments
@@ -39,16 +56,9 @@ class App extends React.Component {
   // ]
   // }
 
-  // declare function addCompartment to add a new compartment array to the storage object
-  // to be used on compartment input button click
 
   // declare function delCompartment to remove a compartment array
   // to be used on compartment X button click
-
-  // declare function addListItem to add a new list item object to the storage object / compartment array
-  // to be used on list input button click (add to buttons: onClick={this.addListItem.bind(this)}) ???
-  // this.setState({itemCount: this.state.itemCount + 1});
-  // add list item object to compartment's array
 
   // declare function delListItem to remove a list item object from the storage object / compartment array
   // to be used on list X button click
@@ -61,11 +71,12 @@ class App extends React.Component {
         {/* render buttons that will setState of itemView */}
         {
           // generate buttons dynamically using props.pages
-          this.viewButtons.map((item) => {
+          this.viewButtons.map((item, key) => {
             return (
               <button
                 className={'m-2 btn ' + ((this.state.itemView === item) ? ' btn-primary' : 'btn-secondary')}
                 onClick={() => this.setView(item)}
+                key={key}
               >
                 {item}
               </button>
@@ -73,15 +84,17 @@ class App extends React.Component {
           })
         }
         {/* COMPARTMENT INPUT onClick=addCompartment */}
-        <div class="input-group p-3">
-          <input type="text" class="form-control" placeholder="add new compartment" aria-label="Recipient's username" aria-describedby="button-addon2"></input>
-          <div class="input-group-append">
-            <button class="btn btn-outline-secondary" type="button" id="button-addon2">+</button>
+        <div className='input-group p-3'>
+          <input id='compartmentInput' type='text' className='form-control' placeholder='add new compartment' aria-label='Recipient&apos;s username' aria-describedby='button-addon2'></input>
+          <div className='input-group-append'>
+            <button className='btn btn-outline-secondary' type='button' id='button-addon2' onClick={() => this.addCompartment(document.getElementById('compartmentInput').value)}>+</button>
           </div>
+          {/* <div className='input-group-append'>
+            <button className='btn btn-outline-secondary' type='button' id='button-addon2' onClick={() => this.addListItem(document.getElementById('compartmentInput').value)}>-</button>
+          </div> */}
         </div>
-        {/* 
 
-        for each compartment in the data object:
+        {/* for each compartment in the data object:
             <Compartment
               itemView={this.state.itemView}
               compartmentName={name}
@@ -89,8 +102,8 @@ class App extends React.Component {
               addListItem={(???) => this.addListItem(???)}
               delListItem={(???) => this.delListItem(???)}
               delCompartment={(???) => this.delCompartment(???)}
-              />
-        */}
+              /> */}
+
       </>
     );
   }
